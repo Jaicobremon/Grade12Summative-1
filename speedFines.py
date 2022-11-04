88a78cbdc9103e80656a651808015deb942a1ec2
KingJR <71569745+Jaicobremon@users.noreply.github.com>
2022-11-03 21:31:26 -0400

    Add files via upload

---
+++ speedFines.py
@@ -0,0 +1,36 @@
+# Programmed by Jaicob Remon
+# Grade 12 CS summative part 2
+
+# Defining the function
+def fineCalculator():
+
+    #Asking for speed limit
+    speedLimit = input("What was the speed limit on the road ?: ")
+
+    #Asking for the drivers speed
+    recordedSpeed = input("What speed were you driving at ?: ")
+
+    # Calculating wheter the driver was speeding or not
+    difference = int(recordedSpeed) - int(speedLimit)
+
+    # If the driver was not speeding
+    if difference <= 0:
+        print("Congratulations you were within the speed limit")
+    
+    # If the driver was speeding between 1 and 20
+    if difference >= 1 and difference <= 20:
+        print("You were slightly over the speed limit")
+        print("Your fine is $100")
+
+    # If the driver was speeding between 21 and 30
+    if difference >= 21 and difference <= 30:
+        print("You were over the speed limit")
+        print("Your fine is $270")
+
+    # If the driver was speeding above 31
+    if difference >= 31:
+        print("You were way above the speed limit")
+        print("Your fine is $500")
+
+# Calling the function
+fineCalculator()
---
+++ tipCalculator.py
@@ -0,0 +1,65 @@
+# Programmed by Jaicob Remon
+# Grade 12 CS Summative part 1
+
+# Creating calculator function
+def tipCalculator():
+    
+    # Asking the user for their level of satisfaction
+    levelOfSatisfaction = input("How satisfied were you with the service today ?:")
+
+    #What to do if the customer is not satisfied
+    if levelOfSatisfaction == "3":
+
+        print("We are sorry to hear that .")
+
+        # Setting the tax to 10%
+        tax = 0.10
+
+        # Asking for the total of the bill
+        billCount = input("How much was your bill ?: ")
+
+        # Calculating the final tip
+        finalTip = float(billCount) * float(tax)
+
+        #What is the tip which needs to be paid
+        print('The tip owed is ---> $' + str(finalTip))
+
+    #What to do if the customer is satisfied
+    if levelOfSatisfaction == "2":
+
+        print("That's good.")
+
+        #Setting the tax to 15%
+        tax = 0.15
+
+        #asking how much was the bill
+        billCount = input("How much was your bill ?: ")
+
+        #Calculating the tip
+        finalTip = float(billCount) * float(tax)
+
+        # What is the tip that needs to be paid
+        print('The tip owed is ---> $' + str(finalTip))
+
+    #What to do if the customer is extremely satisfied
+    if levelOfSatisfaction == "1":
+
+        print("That's great !")
+
+        # Setting the tax to 20%
+        tax = 0.20
+
+        #Asking for the total of the bill
+        billCount = input("How much was your bill ?: ")
+
+        #Calculating the tip
+        finalTip = float(billCount) * float(tax)
+
+        #Printing the tip
+        print('The tip owed is ---> $' + str(finalTip))
+
+#Calling the function
+tipCalculator()
+
+# Thanking the customer
+print("Thank you for dining with us !")
